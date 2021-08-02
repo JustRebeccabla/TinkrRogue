5b2a15b4528a9d63c469935a9c90f02b5c60b26e
JustRebeccabla <46432043+JustRebeccabla@users.noreply.github.com>
2021-08-02 22:49:32 +0200

    Update _loader.lua

--- _loader.lua
+++ _loader.lua
@@ -2,7 +2,13 @@ local Tinkr = ...
 local Routine = Tinkr.Routine
 
 if UnitClass("player") == "Rogue" then
-  --Eval('RunMacroText("/tinkr load cromulon.luac")', 'r')
   Eval('RunMacroText("/tinkr load BeccaRogue.lua")', 'r')
+  --Eval('RunMacroText("/tinkr load BeccaGatherEsp.lua")', 'r')
+  --Eval('RunMacroText("/tinkr load BeccaZygorEsp.lua")', 'r')
+  --Eval('RunMacroText("/tinkr load BeccaEsp.lua")', 'r')
+  elseif UnitClass("player") == "Druid" then
+   --Eval('RunMacroText("/tinkr load BeccaDruid.lua")', 'r')
+  --Eval('RunMacroText("/tinkr load BeccaGatherEsp.lua")', 'r')
+  --Eval('RunMacroText("/tinkr load BeccaZygorEsp.lua")', 'r')
   --Eval('RunMacroText("/tinkr load BeccaEsp.lua")', 'r')
 end
